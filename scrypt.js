a24f184c0eea665752c72c5f116e3cc26f7c0feb
fil <filipp.kosenko@1win.pro>
2023-11-17 19:08:57 +0300

    first work

--- scrypt.js
+++ scrypt.js
@@ -1,3 +1,22 @@
-let name = prompt("What you name ?", ['Lion'])
+'use strict';
 
-alert(`You name ${name}`);
+let numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '2');
+
+
+
+let personalMovieDB={
+    count : numberOfFilms,
+    movies : {
+    },
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+let lastWatchFilm = prompt('Один из последних просмотренных фильмов?', '');
+let lastFilmBull = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[lastWatchFilm] = lastFilmBull;
+
+console.log(personalMovieDB);
+
